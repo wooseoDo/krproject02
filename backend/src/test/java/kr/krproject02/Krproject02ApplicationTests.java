9e255f06621572269af47c22f116cd44232d01b7
Peinoi <124995489+Peinoi@users.noreply.github.com>
2026-05-07 16:45:57 +0900

    first commit

---
+++ backend/src/test/java/kr/krproject02/Krproject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.krproject02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Krproject02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
